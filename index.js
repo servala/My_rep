4444948cc475a062d3fca012667aa8871c00e8be
servala <marina.severyanova@gmail.ru>
2012-09-05 13:46:27 +0400

    static site-bem - index -styles1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
         })
         $('.b-menu').on({
             'mouseover': function(e){
-                $(this).css('background', 'url(bkg_menu.gif) no-repeat');
+                $(this).css('background', 'url(images/bkg_menu.gif) no-repeat');
             },
             'mouseout': function(e){
                 $(this).css('background', '');
@@ -89,7 +89,7 @@ $('.e-content-item-bullet' ).on({
                $( '.e-content-item-inner').css( 'display', 'block');
                var buttons = $('<div id="pr"><button onclick="$(\'#pr\' ).hide();window.print();$(\'#pr\' ).show();">Печатать</button>' +
                        '<button onclick="window.location.reload( true );" >Отменить</button></div>')
-               var for_print = $('<div ></div>').html( $( '.b-content').html() );
+               var for_print = $('<div style="padding-bottom: 50px;"></div>').html( $( '.b-content').html() );
                $( 'head').html( ' <style type="text/css" >' +
                        'b, i{ display:block;} i{ font-size: 18px; margin: 15px 0 0 -20px; border-top: 1px dotted black;} ' +
                        'b{padding-top: 10px;} div{width: 800px; margin: 0 auto;}' +
